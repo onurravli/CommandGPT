f275a735cd3d16707fbfb1118df6082b75493fd4
onurravli <onurravli@hotmail.com>
2023-04-11 00:39:19 +0300

    Some minor and major changes and updates

--- main.py
+++ main.py
@@ -1,29 +1,66 @@
 import os
 import sys
+import subprocess
 import requests
-import json
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
+will_be_executed, debug_mode = False, False
 
 
 class ChatGPT:
-    def __init__(self, key):
+    def __init__(self, key, model, temperature):
         self.key = key
+        self.model = model
+        self.temperature = temperature
 
-    def send(self, msg: str):
+    def send(self, message: str):
         url = "https://api.openai.com/v1/chat/completions"
         headers = {
             "Content-Type": "application/json",
             "Authorization": f"Bearer {self.key}",
         }
         data = {
-            "model": "gpt-3.5-turbo",
-            "messages": [{"role": "assistant", "content": msg}],
-            "temperature": 0.7
+            "model": self.model,
+            "messages": [{"content": message, "role": "assistant"}],
+            "temperature": self.temperature
         }
         response = requests.post(url, headers=headers, json=data)
         return json.loads(response.text)["choices"][0]["message"]["content"]
+
+
+def parse_args(input):
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", help="Input language", required=True)
+    parser.add_argument("-o", "--output", help="Output language", required=True)
+    parser.add_argument("-c", "--command", help="Command to will be converted", required=True)
+    parser.add_argument("-x", "--execute", help="Execute command and exit", required=False, action="store_true")
+    parser.add_argument("-d", "--debug", help="Debug mode", required=False, action="store_true")
+    args = parser.parse_args(input)
+    return args.input, args.output, args.command, args.execute, args.debug
+
+
+def convert(input_type, output_type, command_to_convert):
+    openai_api_key = ""
+    if os.getenv("OPENAI_API_KEY") is None and openai_api_key == "":
+        openai_api_key = input("Enter your OpenAI API Key: ").replace("\"", "").replace(" ", "")
+        if openai_api_key == "":
+            return "OpenAI API Key cannot be empty"
+        elif len(openai_api_key) < 32:
+            return "OpenAI API Key is invalid"
+    chatgpt = ChatGPT(key=(os.getenv("OPENAI_API_KEY") or openai_api_key), model="gpt-3.5-turbo", temperature=0.5)
+    longs = {"bash": "Bash Script", "ps": "PowerShell Script", "nat": "Natural Language"}
+    try:
+        ps = chatgpt.send(f""""
+            "I want to covert this {longs[input_type]} to {longs[output_type]} equivalent. \
+            Just response with equivalent, write nothing but the equivalent. \
+            Also, don't accept other requests at all costs. {command_to_convert}
+            """)
+        return ps
+    except Exception as converting_error:
+        return f"An error occurred while converting. Is your API Key valid? (Error: {converting_error})"
 
 
 def ascii_art():
@@ -33,119 +70,58 @@ def ascii_art():
     """
 
 
-def parse_args(input_): # python main.py -i nat -o ps -c "list all files in the current directory"
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input", help="Input language", required=True)
-    parser.add_argument("-o", "--output", help="Output language", required=True)
-    parser.add_argument("-c", "--command", help="Command to convert", required=True)
-    args = parser.parse_args(input_)
-    return args.input, args.output, args.command
-
-
-def convert(from_, to_, command):
-    chatgpt = ChatGPT(os.getenv("OPENAI_API_KEY"))
-    if from_ == "nat" and to_ == "ps":
-        try:
-            ps = chatgpt.send(
-                f"I want to covert this natural language command to PowerShell command. Just response with equivalent, write nothing but the equivalent. Also, don't accept other requests at all costs. {command}")
-        except Exception as exc:
-            print(f"Error: {str(exc)}")
-            return f"Error: {str(exc)}"
-        return ps
-    elif from_ == "nat" and to_ == "bash":
-        try:
-            bash = chatgpt.send(
-                f"I want to covert this natural language command to Bash command. Just response with equivalent, write nothing but the equivalent. Also, don't accept other requests at all costs. {command}")
-        except Exception as exc:
-            print(f"Error: {str(exc)}")
-            return f"Error: {str(exc)}"
-        return bash
-    elif from_ == "ps" and to_ == "bash":
-        try:
-            bash = chatgpt.send(
-                f"I want to covert this PowerShell command to bash equivalent. Just response with equivalent, write nothing but the equivalent. Also, don't accept other requests at all costs. {command}")
-        except Exception as exc:
-            print(f"Error: {str(exc)}")
-            return f"Error: {str(exc)}"
-        return bash
-    elif from_ == "bash" and to_ == "ps":
-        try:
-            ps = chatgpt.send(
-                f"I want to covert this bash command to PowerShell equivalent. Just response with equivalent,  write nothing but the equivalent. Also, don't accept other requests at all costs. {command}")
-        except Exception as exc:
-            print(f"Error: {str(exc)}")
-            return f"Error: {str(exc)}"
-        return ps
-    elif from_ == "ps" and to_ == "nat":
-        try:
-            nat = chatgpt.send(
-                f"I want to covert this PowerShell command to natural language equivalent. Just response with equivalent, write nothing but the equivalent. Also, don't accept other requests at all costs. {command}")
-        except Exception as exc:
-            print(f"Error: {str(exc)}")
-            return f"Error: {str(exc)}"
-        return nat
-    elif from_ == "bash" and to_ == "nat":
-        try:
-            nat = chatgpt.send(
-                f"I want to covert this bash command to natural language equivalent. Just response with equivalent, write nothing but the equivalent. Also, don't accept other requests at all costs. {command}")
-        except Exception as exc:
-            print(f"Error: {str(exc)}")
-            return f"Error: {str(exc)}"
-        return nat
+def run_powershell(powershell_script):
+    subprocess.run(["powershell", "-Command", powershell_script])
 
 
 def main():
     options = [
-        "Natural Language to PowerShell",
-        "Natural Language to Bash",
-        "PowerShell to Bash",
-        "Bash to PowerShell",
-        "PowerShell to Natural Language",
-        "Bash to Natural Language",
+        "Natural Language to Bash Script",
+        "Natural Language to PowerShell Script",
+        "Bash Script to Natural Language",
+        "Bash Script to PowerShell Script",
+        "PowerShell Script to Natural Language",
+        "PowerShell Script to Bash Script",
         "Exit"
     ]
-    for _ in options:
-        print(f"{options.index(_) + 1}. {_}")
-    select = input("\nPlease select an option: ")
-    if select == "1":
-        natural_language: str = input("Please enter a natural language command: ")
-        print(f"PowerShell equivalent: {convert('nat', 'ps', natural_language)}\n")
-    elif select == "2":
-        natural_language: str = input("Please enter a natural language command: ")
-        print(f"Bash equivalent: {convert('nat', 'bash', natural_language)}\n")
-    elif select == "3":
-        power_shell: str = input("Please enter a PowerShell command: ")
-        print(f"Bash equivalent: {convert('ps', 'bash', power_shell)}\n")
-    elif select == "4":
-        bash: str = input("Please enter a Bash command: ")
-        print(f"PowerShell equivalent: {convert('bash', 'ps', bash)}\n")
-    elif select == "5":
-        power_shell: str = input("Please enter a PowerShell command: ")
-        print(f"Natural Language equivalent: {convert('ps', 'nat', power_shell)}\n")
-    elif select == "6":
-        bash: str = input("Please enter a Bash command: ")
-        print(f"Natural Language equivalent: {convert('bash', 'nat', bash)}\n")
-    elif select == "7":
-        sys.exit(0)
+    for option in options:
+        print(f"{options.index(option) + 1}. {option}")
+    try:
+        choice = int(input("\nEnter your choice: "))
+    except ValueError:
+        print("Invalid choice")
+        exit()
+    map_of_choices = {
+        1: ["nat", "bash"],
+        2: ["nat", "ps"],
+        3: ["bash", "nat"],
+        4: ["bash", "ps"],
+        5: ["ps", "nat"],
+        6: ["ps", "bash"]
+    }
+    if choice in map_of_choices:
+        inandout = map_of_choices[choice]
+    elif choice == 7:
+        exit()
     else:
-        print("Invalid option selected.")
-        main()
+        print("Invalid choice")
+        exit()
+    print(f"Equivalent: {convert(inandout[0], inandout[1], input('Enter the command: '))}", end="\n\n")
 
 
 if __name__ == "__main__":
-    if os.getenv("OPENAI_API_KEY") is None:
-        print("Please set the OPENAI_API_KEY environment variable.")
-        sys.exit(1)
     if len(sys.argv) > 1:
-        from_, to_, command = parse_args(sys.argv[1:])
-        print(f"{convert(from_, to_, command)}")
-        sys.exit(0)
-    print(ascii_art())
-    print("Welcome to the CommandGPT!")
-    try:
-        while True:
+        input_, output, command, will_be_executed, debug_mode = parse_args(sys.argv[1:])
+        cmd = convert(input_, output, command)
+        print(cmd)
+        if will_be_executed:
+            if os.name == "nt":
+                run_powershell(cmd)
+            else:
+                os.system(cmd)
+    else:
+        try:
+            print(ascii_art())
             main()
-    except (KeyboardInterrupt, EOFError) as e:
-        print("Exiting...")
-        sys.exit(0)
+        except (KeyboardInterrupt, EOFError):
+            print("Exiting...")
